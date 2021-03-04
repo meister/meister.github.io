979da005f8fbad99b4bebc721a6905f551a1e576
Martin Kapp <martin@coderats.net>
2021-03-04 23:28:26 +0200

    Update workflow and build file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+outdir=${RUNNER_WORKSPACE:-.}
+
 npx meister |
 	sed -E 's/\[39m/<\/span>/g' |
 	sed -E 's/\[90m/<span class="color-90">/g' |
-	sed -E 's/\[38\;5\;([0-9]+)m/<span class="color-\1">/g' > index.html
+	sed -E 's/\[38\;5\;([0-9]+)m/<span class="color-\1">/g' > "$outdir"/index.html
