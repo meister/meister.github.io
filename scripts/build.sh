419b42e9ac95d65f8aa9dc5314865095088c0dce
Martin Kapp <martin@coderats.net>
2021-03-05 13:50:01 +0200

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ cat "${workdir}/_top.md" > "${workdir}/index.md"
 npx meister |
 	sed '/^$/d' |
 	sed 's/[[:blank:]]*$//' |
-	sed 's|$|<br/>|' |
+	# sed 's|$|<br/>|' |
 	sed -E 's/\[39m/<\/span>/g' |
 	sed -E 's/\[90m/<span class="color-90">/g' |
 	sed -E 's/\[38\;5\;([0-9]+)m/<span class="color-\1">/g' \
