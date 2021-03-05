a89fcdf159b80d922c3b99003ffd9d3f14f022c3
Martin Kapp <martin@coderats.net>
2021-03-05 11:10:38 +0200

    Improve parser script

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,10 @@
 outdir=${GITHUB_WORKSPACE:-.}
 
 npx meister |
+	sed '/^$/d' |
+	sed 's/[[:blank:]]*$//' |
+	sed 's|$|<br/>|' |
 	sed -E 's/\[39m/<\/span>/g' |
 	sed -E 's/\[90m/<span class="color-90">/g' |
-	sed -E 's/\[38\;5\;([0-9]+)m/<span class="color-\1">/g' > "$outdir"/index.html
+	sed -E 's/\[38\;5\;([0-9]+)m/<span class="color-\1">/g' \
+	> "$outdir"/index.html
